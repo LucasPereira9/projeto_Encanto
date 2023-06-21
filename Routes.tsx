172ba2f6cb68ba16a859f14d9349ed2aa6e3d024
Lucas Pereira de Almeida <lucas.qesh@MacBook-Air-de-Lucas.local>
2023-06-21 12:49:57 -0300

    :sparkles: feat: colors theme added

--- Routes.tsx
+++ Routes.tsx
@@ -1,27 +1,31 @@
 import * as React from 'react';
 import 'react-native-gesture-handler';
-
-import {createStackNavigator} from '@react-navigation/stack';
-import Home from './src/pages/Home';
-import {NavigationContainer} from '@react-navigation/native';
-import Login from './src/pages/Login';
 import {CheckpointProvider} from './src/hooks/Checkpoint';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import Home from './src/pages/Home';
+import Login from './src/pages/Login';
+import {ThemeProvider} from 'styled-components';
+
 import Register from './src/pages/Register';
+import theme from './src/global/theme';
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
-    <NavigationContainer>
-      <CheckpointProvider>
-        <Stack.Navigator
-          screenOptions={{headerShown: false}}
-          initialRouteName={'Login'}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Register" component={Register} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </CheckpointProvider>
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <CheckpointProvider>
+          <Stack.Navigator
+            screenOptions={{headerShown: false}}
+            initialRouteName={'Login'}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+        </CheckpointProvider>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,10 +3,11 @@ import {View, Text} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {useNavigation} from '@react-navigation/native';
 import auth from '@react-native-firebase/auth';
+import theme from '../../global/theme';
 
 export default function Home() {
   const navigation = useNavigation();
-  console.log(auth().currentUser);
+  console.log(auth().currentUser, theme);
   return (
     <View>
       <TouchableOpacity onPress={() => navigation.navigate('Register')}>
