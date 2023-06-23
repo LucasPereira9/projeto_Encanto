9770395eadc6261e024e823e71c2952f597fa19a
Lucas Pereira de Almeida <lucas.qesh@MacBook-Air-de-Lucas.local>
2023-06-23 15:42:54 -0300

    :sparkles: feat: register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {Animated, Text, View} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import LinearGradient from 'react-native-linear-gradient';
 import theme from '../../global/theme';
 import {defaultStyles} from '../../global/defaultStyles';
+import Input from '../../components/input';
+import {styles} from './styles';
+import Button from '../../components/button';
 
 export default function Register() {
+  const Show = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(Show, {
+      duration: 2000,
+      toValue: 1,
+      delay: 200,
+      useNativeDriver: false,
+    }).start();
+  }, [Show]);
   return (
     <View style={defaultStyles.Container}>
+      <Animated.View style={{opacity: Show}}>
+        <View style={styles.content}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>CRIAR NOVA CONTA</Text>
+          </View>
+          <View>
+            <Input icon="user" placeholder="insira seu nome" />
+            <Input icon="mail" placeholder="insira o email" />
+            <Input icon="lock" placeholder="insira a senha" secureText={true} />
+            <Input
+              icon="lock"
+              placeholder="confirme a senha"
+              secureText={true}
+            />
+          </View>
+        </View>
+        <View style={styles.button}>
+          <Button title="CRIAR CONTA" pressed={() => console.log('clicou')} />
+        </View>
+      </Animated.View>
+
       <LinearGradient
         colors={['transparent', theme.colors.primary]}
         start={{x: 0, y: 0}}
--- src/pages/Register/styles.tsx
+++ src/pages/Register/styles.tsx
@@ -1,4 +1,22 @@
 import {StyleSheet} from 'react-native';
 import theme from '../../global/theme';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  content: {
+    height: '90%',
+    justifyContent: 'space-around',
+  },
+  titleContainer: {
+    alignItems: 'center',
+    top: '13%',
+  },
+  title: {
+    fontSize: 22,
+    color: theme.colors.primary,
+  },
+  button: {
+    alignContent: 'center',
+    alignItems: 'center',
+    bottom: '22%',
+  },
+});
